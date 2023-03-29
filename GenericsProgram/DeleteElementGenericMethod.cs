690750750a4a298bc3c0becafe9fd86d8ea1e66c
akshata <akshata2018p@gamil.com>
2023-03-29 22:26:10 +0530

    [akshata]Add:Check Delete Element Generic Method

---
+++ GenericsProgram/DeleteElementGenericMethod.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsProgram
+{
+    public class DeleteElementGenericMethod
+    {
+        public static void DeleteElement<T>(T[] arr, T delete)
+        {
+            int k = 0;
+            T[] result = new T[arr.Length - 1];
+            for (var i = 0; i < arr.Length; i++)
+            {
+                if (!delete.Equals(arr[i]))
+                {
+                    result[k] = arr[i];
+                    k++;
+                }
+            }
+            Console.WriteLine("Modified elements ;");
+            foreach (var j in result)
+            {
+                Console.WriteLine(j + "");
+            }
+        }
+    }
+}
